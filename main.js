3b34f58cc221fad7f9aaf8d131c95db595a723ff
lucapu88 <caputoluca88@gmail.com>
2019-11-19 15:11:23 +0100

    prima fase, stampato numeri fizz e buzz insieme

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// Stampare tutti i numeri da 1 a 100 con le seguenti regole:
+// al posto dei multipli di 3 stampare "Fizz"
+// al posto dei multipli di 5 stampare "Buzz"
+// al posto dei multipli di 3 e 5 stampare "FizzBuzz"
 
+// var fizz = (i % 3 == 0);
+// var buzz = (i % 5 == 0);
+
+for (var i = 1; i <= 100; i++) {
+ console.log(i);
+ if (i % 3 == 0) {
+   console.log('Fizz');
+ } else if (i % 5 == 0) {
+   console.log('Buzz');
+ } else if ((i % 3 == 0) && (i % 5 == 0)) {
+   console.log('FizzBuzz'); //non funziona
+ }
+}
